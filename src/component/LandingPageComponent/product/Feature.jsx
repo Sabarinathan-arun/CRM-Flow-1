ad1c25ee2efd168ed1d29369a882d6c8737d442e
crmflowcom <crmflow.help@gmail.com>
2025-06-17 21:18:01 +0530

    Update Feature.jsx

--- src/component/LandingPageComponent/product/Feature.jsx
+++ src/component/LandingPageComponent/product/Feature.jsx
@@ -10,7 +10,7 @@ const ProductFeature = () => {
     {
       title: "Seamless Analytics Integration",
       description:
-        "Understand customer behavior instantly with real-time CRM analytics. Our custom CRM helps small businesses increase sales by offering data-driven insights. Monitor key performance indicators and lead trends to improve decision-making and grow faster.",
+        "Understand customer behaviour instantly with real-time CRM analytics. Our custom CRM helps small businesses increase sales by offering data-driven insights. Monitor key performance indicators and lead trends to improve decision-making and grow faster.",
       image: Analytic
     },
     {
@@ -22,13 +22,13 @@ const ProductFeature = () => {
     {
       title: "Customizable Dashboards",
       description:
-        "Tailor your dashboard to focus on what matters most—leads, conversions, sales pipelines, and team performance. With our custom CRM, small businesses can track lead management and increase efficiency using a clean, powerful interface.",
+        "Tailor your dashboard to focus on what matters most: leads, conversions, sales pipelines, and team performance. Our custom CRM allows small businesses to track lead management and increase efficiency using a clean, powerful interface."
       image: Dash
     },
     {
       title: "Real-Time Collaboration",
       description:
-        "Boost productivity with real-time team collaboration. CRM Flow supports instant updates, task sharing, and communication across teams—making it the best CRM software for real estate, startups, and growing teams who need speed and simplicity.",
+        "Boost productivity with real-time team collaboration. CRM Flow supports instant updates, task sharing, and communication across teams by making it the best CRM software for real estate, startups, and growing teams who need speed and simplicity."
       image: Real
     }
   ];
